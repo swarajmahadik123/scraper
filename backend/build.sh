a80c604837b2ec4fb31de7c068b80cd1528d4d3c
swarajmahadik123 <swarajmahadik1234@gmail.com>
2025-01-05 10:12:11 +0530

    Create build.sh

---
+++ backend/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Install system dependencies
+curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
+apt-get -y update
+apt-get -y install google-chrome-stable
+
+# Install ChromeDriver
+CHROME_DRIVER_VERSION=$(curl -sS https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
+wget -N https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+chmod +x chromedriver
+mv -f chromedriver /usr/local/bin/chromedriver
+rm chromedriver_linux64.zip
+
+# Install Node.js dependencies
+npm install
